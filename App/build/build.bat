2943aec361a65808e761614e69fbd915505a2022
Dylan <kdeece@gmail.com>
2014-05-04 20:10:11 -0600

    Fix directory reference when building outside of build directory and overwrite minified files. Ignore node modules directory properly

--- App/build/build.bat
+++ App/build/build.bat
@@ -5,18 +5,20 @@ set css=%public%\css
 set stamp=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%TIME:~0,2%%TIME:~3,2%
 
 echo Compiling shared templates
+pushd %~dp0
 node shared_compiler.js
+popd
 
 echo Minifying and combining js
 pushd %js%
 del /F dist\*.min.js
-ajaxmin app.js templates\feed.js templates\feedlist.js models\feedlist.js models\feed.js views\pages\search.js views\picklist.js views\feed.js views\feedlist.js views\pin-form.js -out dist\feed.min.js
+ajaxmin app.js templates\feed.js templates\feedlist.js models\feedlist.js models\feed.js views\pages\search.js views\picklist.js views\feed.js views\feedlist.js views\pin-form.js -out dist\feed.min.js -clobber
 popd
 
 echo Minifying and combining css
 pushd %css%
 del /F dist\*.min.css
-ajaxmin style.css bootstrap.min.css -out dist\feed.min.css
+ajaxmin style.css bootstrap.min.css -out dist\feed.min.css -clobber
 popd
 
 pause
